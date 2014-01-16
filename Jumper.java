1133b5f7502905e6f659946d1c9258890cad2997
Madden <mopheadhippie@gmail.com>
2014-01-16 09:31:23 -0700

    added code

--- Jumper.java
+++ Jumper.java
@@ -1,6 +1,34 @@
-public class Jumper extends Bug {
+public class Jumper extends Actor {
 	
 	public static void main(String[] args) {
 		
-	}
+	} 
+
+	public boolean CanJump() 
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return false;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (!gr.isValid(next))
+            return false;
+        Actor neighbor = gr.get(next);
+        return (neighbor == null) || (neighbor instanceof Flower);
+    }
+
+    public void jump(){
+    	Grid<Actor> gr = getGrid();
+    	if (gr == null) {
+    		return;
+    	}
+    	Location loc = next.getLocation();
+    	Location next = loc.getAdjacentLocation(getDirection());
+
+    	if (gr.isValid) {
+    		move();
+    	}
+    }
+
+
 }
